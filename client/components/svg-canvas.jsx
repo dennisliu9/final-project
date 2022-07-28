d57fb673295e00182cf72c6270d0976e3471c9f8
Dennis Liu <dennis.liu9@gmail.com>
2022-07-28 18:10:44 +0000

    Created component that creates paths from canvas props, logo now renders conditionally

--- client/components/svg-canvas.jsx
+++ client/components/svg-canvas.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
+import LogoSplash from '../components/logo-splash';
+import DrawnPath from './drawn-path';
+
+const AppContext = React.createContext();
+const svgNS = 'http://www.w3.org/2000/svg';
+
+// For drawing paths, the canvas records the mouse locations and saves it to a state
+// DrawnPaths are rendered by a .map() across all of the recorded path details in the state
+// To update the array in the state, use setState with [...currentArray, newArray]
 
 export default class SVGCanvas extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nextElementId: 0,
+      drawnPaths: [
+        {
+          elementId: 1,
+          startingPoint: [10, 10],
+          pathData: [
+            [150, 150],
+            [250, 300],
+            [350, 450],
+            [520, 229]
+          ],
+          stroke: 'hsl(204, 86%, 53%)',
+          strokeWidth: 5
+        }
+      ]
+    };
+  }
+
   render() {
+    const contextValue = { svgNS };
+
     return (
-      <svg id="svg-canvas" className="page-center">
-      </svg>
+      <AppContext.Provider value={contextValue}>
+        <div className="page-center">
+          <svg id="svg-canvas" className="page-center" xmlns={contextValue.svgNS}>
+            { this.state.drawnPaths.map(
+              pathDetail =>
+                <DrawnPath
+                  key={pathDetail.elementId}
+                  startingPoint={pathDetail.startingPoint}
+                  pathData={pathDetail.pathData}
+                  stroke={pathDetail.stroke}
+                  strokeWidth={pathDetail.strokeWidth}
+                />
+            )}
+          </svg>
+          {(this.state.drawnPaths.length > 0) ? <></> : <LogoSplash />}
+        </div>
+      </AppContext.Provider>
     );
   }
 }
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-// import HelloWorld from '../components/hello-world';
 import SVGCanvas from '../components/svg-canvas';
-import LogoSplash from '../components/logo-splash';
 
 export default function Home(props) {
   return (
     <div className="page-center">
-      <LogoSplash />
       <SVGCanvas />
     </div>
   );
