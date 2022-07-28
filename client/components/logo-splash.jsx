9f3cd59bfc8c09cd8b6f5b5fc5631d6acdb59f9b
Dennis Liu <dennis.liu9@gmail.com>
2022-07-28 22:57:34 +0000

    Added event handlers to allow user to draw with mouse, modified logo splash to display Tap or Click based on mobile or desktop breakpoints

--- client/components/logo-splash.jsx
+++ client/components/logo-splash.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function LogoSplash() {
   return (
-    <div id="logo-splash" className="has-text-centered has-text-grey-lighter">
-      <h1 className="is-size-1 has-text-weight-bold">ScratchPad</h1>
-      <h3 className="is-size-3 ">Tap anywhere to start drawing!</h3>
+    <div id='logo-splash' className='has-text-centered has-text-grey-lighter'>
+      <h1 className='is-size-1 has-text-weight-bold'>ScratchPad</h1>
+      <h3 className='is-size-3 '><span className='is-hidden-desktop'>Tap</span><span className='is-hidden-touch'>Click</span> anywhere to start drawing!</h3>
     </div>
   );
 }
--- client/components/svg-canvas.jsx
+++ client/components/svg-canvas.jsx
@@ -2,56 +2,128 @@ import React from 'react';
 import LogoSplash from '../components/logo-splash';
 import DrawnPath from './drawn-path';
 
-const AppContext = React.createContext();
 const svgNS = 'http://www.w3.org/2000/svg';
-
-// For drawing paths, the canvas records the mouse locations and saves it to a state
-// DrawnPaths are rendered by a .map() across all of the recorded path details in the state
-// To update the array in the state, use setState with [...currentArray, newArray]
 
 export default class SVGCanvas extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      nextElementId: 0,
+      nextElementId: 1,
+      currentElementId: null,
+      strokeColor: 'hsl(204, 86%, 53%)',
+      strokeWidth: 5,
       drawnPaths: [
-        {
-          elementId: 1,
-          startingPoint: [10, 10],
-          pathData: [
-            [150, 150],
-            [250, 300],
-            [350, 450],
-            [520, 229]
-          ],
-          stroke: 'hsl(204, 86%, 53%)',
-          strokeWidth: 5
-        }
+        // Example drawnPath object
+        // {
+        //   elementId: 0,
+        //   startingPoint: [10, 10],
+        //   pathData: [
+        //     [150, 150],
+        //     [250, 300],
+        //     [350, 450],
+        //     [520, 229]
+        //   ],
+        //   stroke: 'hsl(204, 86%, 53%)',
+        //   strokeWidth: 5
+        // }
       ]
     };
+    this.addCoordinateToPathData = this.addCoordinateToPathData.bind(this);
+    this.handleMousedown = this.handleMousedown.bind(this);
+    this.handleMousemove = this.handleMousemove.bind(this);
+    this.handleMouseup = this.handleMouseup.bind(this);
+  }
+
+  addCoordinateToPathData(coordinateArr, drawnPathsIdx) {
+    // Retrieve object that we want to add the coordinate to
+    const modifyPathObj = this.state.drawnPaths[drawnPathsIdx];
+
+    // Make a shallow copy of the coordinates it currently has
+    const newPathData = [...modifyPathObj.pathData];
+    newPathData.push(coordinateArr);
+
+    // Recreate the object, substituting the new path data
+    const newCurrentPath = {
+      elementId: modifyPathObj.elementId,
+      startingPoint: modifyPathObj.startingPoint,
+      pathData: newPathData,
+      stroke: modifyPathObj.stroke,
+      strokeWidth: modifyPathObj.strokeWidth
+    };
+
+    // Put the new object back in the original location
+    this.setState({
+      drawnPaths: [...this.state.drawnPaths.slice(0, drawnPathsIdx), newCurrentPath, ...this.state.drawnPaths.slice(drawnPathsIdx + 1)]
+    });
+  }
+
+  handleMousedown(event) {
+    // Get location where user clicked
+    const mouseLocation = [event.clientX, event.clientY];
+
+    // Create a new drawnPath object with user click as startingPoint
+    const newDrawnPath = {
+      elementId: this.state.nextElementId,
+      startingPoint: mouseLocation,
+      pathData: [],
+      stroke: this.state.strokeColor,
+      strokeWidth: this.state.strokeWidth
+    };
+
+    // Add new path to array of paths, and update element ID's
+    this.setState({
+      nextElementId: this.state.nextElementId + 1,
+      currentElementId: this.state.nextElementId,
+      drawnPaths: [...this.state.drawnPaths, newDrawnPath]
+    });
+  }
+
+  handleMousemove(event) {
+    if (this.state.currentElementId !== null) {
+      const mouseLocation = [event.clientX, event.clientY];
+      const currentPathIdx = this.state.drawnPaths.findIndex(element => element.elementId === this.state.currentElementId);
+
+      this.addCoordinateToPathData(mouseLocation, currentPathIdx);
+    }
+
+  }
+
+  handleMouseup(event) {
+    const currentPathIdx = this.state.drawnPaths.findIndex(element => element.elementId === this.state.currentElementId);
+    if (this.state.drawnPaths[currentPathIdx].pathData.length === 0) {
+      // If mouse was clicked but no mouse movement was done, draw a dot.
+      const mouseLocation = [event.clientX, event.clientY];
+      this.addCoordinateToPathData(mouseLocation, currentPathIdx);
+    }
+    this.setState({
+      currentElementId: null
+    });
   }
 
   render() {
-    const contextValue = { svgNS };
-
     return (
-      <AppContext.Provider value={contextValue}>
-        <div className="page-center">
-          <svg id="svg-canvas" className="page-center" xmlns={contextValue.svgNS}>
-            { this.state.drawnPaths.map(
-              pathDetail =>
-                <DrawnPath
-                  key={pathDetail.elementId}
-                  startingPoint={pathDetail.startingPoint}
-                  pathData={pathDetail.pathData}
-                  stroke={pathDetail.stroke}
-                  strokeWidth={pathDetail.strokeWidth}
-                />
-            )}
-          </svg>
-          {(this.state.drawnPaths.length > 0) ? <></> : <LogoSplash />}
-        </div>
-      </AppContext.Provider>
+      <div className="page-center">
+        <svg
+          id="svg-canvas"
+          className="page-center"
+          xmlns={svgNS}
+          onMouseDown={this.handleMousedown}
+          onMouseMove={this.handleMousemove}
+          onMouseUp={this.handleMouseup}
+        >
+          { this.state.drawnPaths.map(
+            pathDetail =>
+              <DrawnPath
+                key={pathDetail.elementId}
+                startingPoint={pathDetail.startingPoint}
+                pathData={pathDetail.pathData}
+                stroke={pathDetail.stroke}
+                strokeWidth={pathDetail.strokeWidth}
+              />
+          )}
+        </svg>
+        {(this.state.drawnPaths.length > 0) ? <></> : <LogoSplash />}
+      </div>
     );
   }
 }
