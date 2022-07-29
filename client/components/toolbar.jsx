ec2c21ed5349dffcc1de49ded1f43b5fe5220ef8
Dennis Liu <dennis.liu9@gmail.com>
2022-07-29 20:01:25 +0000

    Added CSS for positioning toolbar, toolbar buttons now set Home state

--- client/components/toolbar.jsx
+++ client/components/toolbar.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 
 export default class Toolbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  handleButtonClick(event) {
+    const selectedTool = event.currentTarget.dataset.tool;
+    this.props.updateCurrentTool(selectedTool);
+  }
+
   render() {
     return (
-      // <div className="field has-addons"></div>
-      <div className="field has-addons">
-        <p className="control">
+      <div id='toolbar' className="field has-addons is-position-fixed bottom-10pct">
+        <p className="control" data-tool="pen" onClick={this.handleButtonClick}>
           <button className="button is-selected is-primary">
             <span className="icon is-small">
               <i className="fas fa-pen"></i>
             </span>
           </button>
         </p>
-        <p className="control">
+        <p className="control" data-tool="eraser" onClick={this.handleButtonClick}>
           <button className="button">
             <span className="icon is-small">
               <i className="fas fa-eraser"></i>
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -2,11 +2,25 @@ import React from 'react';
 import SVGCanvas from '../components/svg-canvas';
 import Toolbar from '../components/toolbar';
 
-export default function Home(props) {
-  return (
-    <div className="page-center">
-      <SVGCanvas />
-      <Toolbar />
-    </div>
-  );
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTool: 'pen'
+    };
+    this.updateCurrentTool = this.updateCurrentTool.bind(this);
+  }
+
+  updateCurrentTool(toolname) {
+    this.setState({ currentTool: toolname });
+  }
+
+  render() {
+    return (
+      <div className="page-center">
+        <SVGCanvas currentTool={this.state.currentTool} />
+        <Toolbar currentTool={this.state.currentTool} updateCurrentTool={this.updateCurrentTool} />
+      </div>
+    );
+  }
 }
