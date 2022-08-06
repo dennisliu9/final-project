b98d7954c5c9a2be5ed6be478b3c356036c0a64f
Dennis Liu <dennis.liu9@gmail.com>
2022-08-06 00:18:59 +0000

    Updated database schema to better support functionality by removing Strokes and Textboxes tables

---
+++ database/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO "Users" ("username", "email", "dateRegistered", "dateLastLogin")
+VALUES ('test', 'testaccount@melchior.com', NOW(), NOW());
--- database/schema.sql
+++ database/schema.sql
@@ -18,16 +18,12 @@ CREATE TABLE "public"."Users" (
   OIDS=FALSE
 );
 
-
-
 CREATE TABLE "public"."Passwords" (
 	"userId" int NOT NULL,
-	"hashedPassword" TEXT NOT NULL
+	"hashedPassword" TEXT
 ) WITH (
   OIDS=FALSE
 );
-
-
 
 CREATE TABLE "public"."Drawings" (
 	"drawingId" serial NOT NULL,
@@ -35,43 +31,11 @@ CREATE TABLE "public"."Drawings" (
 	"createdByUserId" int NOT NULL,
 	"dateCreated" timestamptz NOT NULL,
 	"dateSaved" timestamptz,
+	"elements" jsonb NOT NULL,
 	CONSTRAINT "Drawings_pk" PRIMARY KEY ("drawingId")
 ) WITH (
   OIDS=FALSE
 );
-
-
-
-CREATE TABLE "public"."Strokes" (
-	"strokeId" serial NOT NULL,
-	"drawingId" serial NOT NULL,
-	"createdByUserId" serial NOT NULL,
-	"dateStartCreated" timestamptz NOT NULL,
-	"dateStopCreated" timestamptz NOT NULL,
-	"strokeElement" TEXT NOT NULL,
-	"strokeAttributes" json NOT NULL,
-	CONSTRAINT "Strokes_pk" PRIMARY KEY ("strokeId")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-CREATE TABLE "public"."TextBoxes" (
-	"textboxId" serial NOT NULL,
-	"drawingId" int NOT NULL,
-	"createdByUserId" int NOT NULL,
-	"dateStartCreated" timestamptz NOT NULL,
-	"dateStopCreated" timestamptz NOT NULL,
-	"isMarkdown" bit NOT NULL,
-	"textboxAttributes" json NOT NULL,
-	"textboxContent" TEXT NOT NULL,
-	CONSTRAINT "TextBoxes_pk" PRIMARY KEY ("textboxId")
-) WITH (
-  OIDS=FALSE
-);
-
-
 
 CREATE TABLE "public"."DrawingSaves" (
 	"userId" int NOT NULL,
@@ -80,18 +44,9 @@ CREATE TABLE "public"."DrawingSaves" (
   OIDS=FALSE
 );
 
-
-
-
 ALTER TABLE "Passwords" ADD CONSTRAINT "Passwords_fk0" FOREIGN KEY ("userId") REFERENCES "Users"("userId");
 
 ALTER TABLE "Drawings" ADD CONSTRAINT "Drawings_fk0" FOREIGN KEY ("createdByUserId") REFERENCES "Users"("userId");
-
-ALTER TABLE "Strokes" ADD CONSTRAINT "Strokes_fk0" FOREIGN KEY ("drawingId") REFERENCES "Drawings"("drawingId");
-ALTER TABLE "Strokes" ADD CONSTRAINT "Strokes_fk1" FOREIGN KEY ("createdByUserId") REFERENCES "Users"("userId");
-
-ALTER TABLE "TextBoxes" ADD CONSTRAINT "TextBoxes_fk0" FOREIGN KEY ("drawingId") REFERENCES "Drawings"("drawingId");
-ALTER TABLE "TextBoxes" ADD CONSTRAINT "TextBoxes_fk1" FOREIGN KEY ("createdByUserId") REFERENCES "Users"("userId");
 
 ALTER TABLE "DrawingSaves" ADD CONSTRAINT "DrawingSaves_fk0" FOREIGN KEY ("userId") REFERENCES "Users"("userId");
 ALTER TABLE "DrawingSaves" ADD CONSTRAINT "DrawingSaves_fk1" FOREIGN KEY ("drawingId") REFERENCES "Drawings"("drawingId");
