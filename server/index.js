13bcb8c791a94553f8dc8d7c958562da9d225d91
Dennis Liu <dennis.liu9@gmail.com>
2022-08-06 07:08:13 +0000

    Added routes for drawing creation and retrievel

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 require('dotenv/config');
 const path = require('path');
 const express = require('express');
+const pg = require('pg');
 const errorMiddleware = require('./error-middleware');
+// const ClientError = require('./client-error');
 
 const app = express();
 const publicPath = path.join(__dirname, 'public');
@@ -12,8 +14,62 @@ if (process.env.NODE_ENV === 'development') {
   app.use(express.static(publicPath));
 }
 
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 app.get('/api/hello', (req, res) => {
   res.json({ hello: 'world' });
+});
+
+app.post('/api/drawings/', (req, res, next) => {
+  const sqlCreateNewDrawing = `
+  INSERT INTO "Drawings" ("urlText", "createdByUserId", "dateCreated", "dateSaved", "elements")
+  -- hard code urlText and createdByUserId for now
+  VALUES ('tbd', '1', NOW(), NULL, '[]')
+  RETURNING "drawingId", "urlText";
+  `;
+
+  db.query(sqlCreateNewDrawing)
+    .then(result => {
+      // const { drawingId, urlText, elements } = result.rows[0];
+      res.json(result.rows[0]);
+    })
+    .catch(err => next(err));
+});
+
+app.get('/api/drawings/:drawingId', (req, res, next) => {
+  const drawingId = req.params.drawingId;
+
+  if (!Number.isInteger(Number(drawingId)) || drawingId < 1) {
+    res.status(400).json({
+      error: `drawingId must be a positive integer, you supplied: ${drawingId}`
+    });
+    return;
+  }
+
+  const sqlGetDrawing = `
+  SELECT  "drawingId", "elements"
+  FROM    "Drawings"
+  WHERE   "drawingId" = $1;
+  `;
+  const params = [drawingId];
+
+  db.query(sqlGetDrawing, params)
+    .then(result => {
+      const [drawingData] = result.rows;
+      if (!drawingData) {
+        res.status(404).json({
+          error: `cannot find drawing with Id = ${drawingId}`
+        });
+        return;
+      }
+      res.json(drawingData);
+    })
+    .catch(err => next(err));
 });
 
 app.use(errorMiddleware);
