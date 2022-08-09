9d8f32203d083ed28ea729f7427b4b6bc2a78b96
Dennis Liu <dennis.liu9@gmail.com>
2022-08-09 18:39:22 +0000

    Changed name/logo to NapkinBack

--- client/components/logo-splash.jsx
+++ client/components/logo-splash.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function LogoSplash() {
   return (
     <div id='logo-splash' className='has-text-centered has-text-grey-lighter'>
-      <h1 className='is-size-1 has-text-weight-bold'>ScratchPad</h1>
+      <h1 className='is-size-1 has-text-weight-bold'>NapkinBack</h1>
       <h3 className='is-size-3 '><span className='is-hidden-desktop'>Tap</span><span className='is-hidden-touch'>Click</span> anywhere to start drawing!</h3>
     </div>
   );
