fb4d87eb67a36eafbf69b8133fa0c17f5aa9984e
Dennis Liu <dennis.liu9@gmail.com>
2022-08-10 10:33:47 +0000

    Added logo to logo splash, logo as favicon, and updated page title to match new name NapkinBack

--- client/components/logo-splash.jsx
+++ client/components/logo-splash.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 export default function LogoSplash() {
   return (
     <div id='logo-splash' className='has-text-centered has-text-grey-lighter'>
-      <h1 className='is-size-1 has-text-weight-bold'>NapkinBack</h1>
-      <h3 className='is-size-3 '><span className='is-hidden-desktop'>Tap</span><span className='is-hidden-touch'>Click</span> anywhere to start drawing!</h3>
+      {/* <h1 className='is-size-1 has-text-weight-bold'>NapkinBack</h1> */}
+      <div className="logo-container m-auto">
+        <img style={{ filter: 'opacity(14%)' }} src='/images/NapkinBack-transparent-300px.png' alt='NapkinBack logo'></img>
+      </div>
+      <h3 className='is-size-3 has-text-weight-bold'><span className='is-hidden-desktop'>Tap</span><span className='is-hidden-touch'>Click</span> anywhere to start drawing!</h3>
     </div>
   );
 }
