28acd3a4b7276fbf92e0e4d01479609b737af5c9
Dennis Liu <dennis.liu9@gmail.com>
2022-08-25 09:22:17 +0000

    Removed unused modules, modified some lines of code to be multi-line for legibility

--- client/components/logo-splash.jsx
+++ client/components/logo-splash.jsx
@@ -4,9 +4,15 @@ export default function LogoSplash() {
   return (
     <div id='logo-splash' className='has-text-centered has-text-grey-lighter'>
       <div className="logo-container m-auto">
-        <img style={{ filter: 'opacity(14%)' }} src='/images/logo-transparent-300px.webp' alt='NapkinNotes logo'></img>
+        <img
+          style={{ filter: 'opacity(14%)' }}
+          src='/images/logo-transparent-300px.webp'
+          alt='NapkinNotes logo'
+        ></img>
       </div>
-      <h3 className='is-size-3 has-text-weight-bold'><span className='is-hidden-desktop'>Tap</span><span className='is-hidden-touch'>Click</span> anywhere to start drawing!</h3>
+      <h3 className='is-size-3 has-text-weight-bold'>
+        <span className='is-hidden-desktop'>Tap</span><span className='is-hidden-touch'>Click</span> anywhere to start drawing!
+      </h3>
     </div>
   );
 }
